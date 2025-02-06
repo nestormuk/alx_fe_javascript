a8e20f08702187b0c0f7faba77453d4c5581d75a
Nestor M <ntor2004@gmail.com>
2025-02-06 10:27:54 +0100

    Add functionality to import quotes from a JSON file

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -71,4 +71,15 @@ createAddQuoteForm();
   
   // Show an initial random quote when the page loads
   showRandomQuote();
+
   
+  function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
